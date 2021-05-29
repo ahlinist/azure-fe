55fb711e197aad6123ffbe7b34219bff351b951a
Anton Hlinisty <anton.hlinisty@gmail.com>
2021-05-29 19:57:41 +0300

    Add port to feign client

--- src/main/java/com/example/demo/client/BackendClient.java
+++ src/main/java/com/example/demo/client/BackendClient.java
@@ -3,7 +3,7 @@ package com.example.demo.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("azure-be")
+@FeignClient("azure-be:8080")
 public interface BackendClient {
 
     @GetMapping("/evening")
