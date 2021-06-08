e332dd781ea7f62bc3d6d07f580824dacf41091b
Anton Hlinisty <anton.hlinisty@gmail.com>
2021-06-08 13:35:07 +0300

    Add be url

--- src/main/java/com/example/demo/client/BackendClient.java
+++ src/main/java/com/example/demo/client/BackendClient.java
@@ -3,7 +3,7 @@ package com.example.demo.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("azure-be:8080")
+@FeignClient(name= "backend", url = "https://azure-fe.azurewebsites.net/")
 public interface BackendClient {
 
     @GetMapping("/evening")
